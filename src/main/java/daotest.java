5b98283d7d1eaab4115e02b5b1e51f40df85ff43
wudongze555 <1504168283@qq.com>
2022-08-25 09:43:18 +0800

    添加了daotest.java和mybatis-config.xml

---
+++ src/main/java/daotest.java
@@ -0,0 +1,14 @@
+import org.apache.ibatis.io.Resources;
+
+/*
+dao测试,利用沙箱将普通方法伪装成main方法执行（junit）
+ */
+public class daotest {
+
+    public void testinsertusers(){
+        //读取单元测试使用的mybatis配置文件
+        String resource ="mybatisJunitTest.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        new
+    }
+}
