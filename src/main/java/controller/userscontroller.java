79d7ed49f6ea03b21df9a3c12d1dfcf3b69f3f33
wudongze555 <1504168283@qq.com>
2022-08-25 11:40:01 +0800

    小更新

---
+++ src/main/java/controller/userscontroller.java
@@ -0,0 +1,11 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//采用sprng-mvc
+@Controller
+@RequestMapping
+public class userscontroller {
+
+}
