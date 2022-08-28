4a55a98366ea1eea2feb48fe7a27a2c2235f0e92
liuzhenda <63899537+liuzhenda@users.noreply.github.com>
2022-08-28 22:16:24 +0800

    添加了列举函数，增加了跳转函数

--- src/main/java/zhenda_liu/controller/UsersController.java
+++ src/main/java/zhenda_liu/controller/UsersController.java
@@ -1,14 +1,23 @@
 package zhenda_liu.controller;
 
 
+import com.google.protobuf.Message;
+import com.google.protobuf.Value;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+import zhenda_liu.domain.Room;
 import zhenda_liu.domain.Users;
 import zhenda_liu.service.UserService;
+import zhenda_liu.utils.MessageAndData;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/Users")
@@ -16,10 +25,30 @@ public class UsersController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = "/register")
-    public void RegisterUsers(Users users){
+/*    @RequestMapping(value = "/register")
+    public ModelAndView RegisterUsers(Users users, HttpServletResponse response) throws IOException {
+        ModelAndView modelAndView = new ModelAndView();
+        PrintWriter out = null;
         boolean b = userService.regNewUser(users);
         System.out.println(b);
+        if(b){
+            modelAndView.setViewName("redirect:../index.html");
+            response.getWriter().write("<script>alert('注册成功!');</script>");
+        }else{
+            modelAndView.setViewName("redirect:../index.html");
+        }
+        return modelAndView;
+    }*/
+
+    @RequestMapping(value = "/register")
+    public void RegisterUsers(Users users, HttpServletResponse response) throws IOException{
+        PrintWriter out = null;
+        boolean b = userService.regNewUser(users);
+        if(b){
+            response.getWriter().write("<script>alert('注册成功!');location.href='../index.html';</script>");
+        }else{
+            response.getWriter().write("<script>alert('用户名重复!');location.href='../register.html';</script>");
+        }
     }
 
 
@@ -35,6 +64,32 @@ public class UsersController {
         }else{
             modelAndView.setViewName("redirect:../index.html");
         }
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/list")
+    @ResponseBody
+    public MessageAndData list(){
+        List<Room> rooms = userService.GetAllRooms();
+        System.out.println("+++++++++++++++++++++");
+        System.out.println(rooms);
+        MessageAndData roomslist = MessageAndData.success().add("roomslist",rooms);
+        return roomslist;
+    }
+
+    @RequestMapping(value = "/JumpToReigster")
+    public ModelAndView JumpToRegister(){
+        ModelAndView modelAndView = new ModelAndView();
+        System.out.println("------------------------------");
+        modelAndView.setViewName("redirect:../register.html");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/JumpToLogin")
+    public ModelAndView JumpToLogin(){
+        ModelAndView modelAndView = new ModelAndView();
+        System.out.println("------------------------------");
+        modelAndView.setViewName("redirect:../index.html");
         return modelAndView;
     }
 }
