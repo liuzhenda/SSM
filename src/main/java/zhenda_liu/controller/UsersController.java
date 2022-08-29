2cb0324e277450c4407d1acbf187480f85e5a1af
liuzhenda <63899537+liuzhenda@users.noreply.github.com>
2022-08-29 21:44:25 +0800

    这个版本的会议室显示会根据用户的部门信息进行相应的显示

--- src/main/java/zhenda_liu/controller/UsersController.java
+++ src/main/java/zhenda_liu/controller/UsersController.java
@@ -5,14 +5,17 @@ import com.google.protobuf.Message;
 import com.google.protobuf.Value;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+import zhenda_liu.domain.Department;
 import zhenda_liu.domain.Room;
 import zhenda_liu.domain.Users;
 import zhenda_liu.service.UserService;
 import zhenda_liu.utils.MessageAndData;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
@@ -69,8 +72,11 @@ public class UsersController {
 
     @RequestMapping(value = "/list")
     @ResponseBody
-    public MessageAndData list(){
-        List<Room> rooms = userService.GetAllRooms();
+    public MessageAndData list(HttpServletRequest request){
+        HttpSession session = request.getSession();
+        Users users = (Users) session.getAttribute("USR_SESSION");
+        users = userService.GetAllUserInfo(users);
+        List<Room> rooms = userService.GetSelectRooms(users);
         System.out.println("+++++++++++++++++++++");
         System.out.println(rooms);
         MessageAndData roomslist = MessageAndData.success().add("roomslist",rooms);
@@ -92,4 +98,44 @@ public class UsersController {
         modelAndView.setViewName("redirect:../index.html");
         return modelAndView;
     }
+
+    @RequestMapping(value = "/list_Department")
+    @ResponseBody
+    public MessageAndData list_Department(){
+        List<Department> departments = userService.GetAllDepartments();
+        MessageAndData roomslist = MessageAndData.success().add("departmentslist",departments);
+        System.out.println(departments);
+        return roomslist;
+    }
+
+    @RequestMapping(value = "/Keep_Login")
+    @ResponseBody
+    public Users Keep_Login(HttpServletRequest request,HttpServletResponse response){
+        HttpSession session = request.getSession();
+        Users users = (Users) session.getAttribute("USR_SESSION");
+        return users;
+    }
+
+
+    @RequestMapping(value = "/opt/{ids}")
+    @ResponseBody
+    public Room FromIdGet(@PathVariable("ids")String ids){
+        int id = Integer.parseInt(ids);
+        System.out.println("this is room=========");
+        System.out.println(id);
+        Room room = userService.GetSelectRoomsByRid(id);
+
+        System.out.println(room);
+        return room;
+    }
+
+
+    //该函数用于用户界面点击退出时，将用户的session信息进行清除
+    @RequestMapping(value = "/Remove_User_Login")
+    @ResponseBody
+    public void Remove_User_Login(HttpServletRequest request){
+        HttpSession session = request.getSession();
+        session.removeAttribute("USR_SESSION");
+    }
+
 }
